ab274844feee77a0c2e695111030de4aa81afd09
Katie Duryea <155897343+katiegd@users.noreply.github.com>
2024-10-11 12:20:46 -0400

    Adjust populatedb.js to not duplicate entries when database is created again on build

--- db/populatedb.js
+++ db/populatedb.js
@@ -17,12 +17,14 @@ async function main() {
 id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY, 
 message VARCHAR(255), 
 name VARCHAR(20),
-created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+UNIQUE(message, name)
 );`);
     await client.query(`INSERT INTO messages (message, name, created_at)
 VALUES
 ('It takes courage to grow up and become who you really are.', 'E.E. Cummings', '2024-10-02 14:21:44.508262' ),
-('It always seems impossible until it''s done.', 'Nelson Mandela', '2024-10-10 14:58:44.502762');
+('It always seems impossible until it''s done.', 'Nelson Mandela', '2024-10-10 14:58:44.502762')
+ON CONFLICT (message, name) DO NOTHING;
 `);
     console.log("Done.");
   } catch (error) {
