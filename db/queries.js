1405d0ec6f9117eb55b3b448e617198badc99f41
Katie Duryea <155897343+katiegd@users.noreply.github.com>
2024-10-11 12:13:27 -0400

    Adjust start script to build database on app build.

--- db/queries.js
+++ db/queries.js
@@ -6,12 +6,10 @@ async function showAllMessages() {
   const { rows } = await pool.query(
     "SELECT * FROM messages ORDER BY created_at DESC"
   );
-  console.log(rows);
   return rows;
 }
 
 async function addNewMessage(newMessage) {
-  console.log("adding new message");
   await pool.query(`INSERT INTO 
         messages (message, name)
          VALUES ('${newMessage.message}', '${newMessage.name}');`);
